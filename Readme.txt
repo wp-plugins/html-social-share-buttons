9e49a3b4442edaaef49a81dec4e8265170638a49
alimuzzamanalim <alimuzzamanalim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 14:04:54 +0000

    Lots of new icons added.
    git-svn-id: https://plugins.svn.wordpress.org/html-social-share-buttons/tags/2.0.4@1074119 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,78 @@
+=== Html Social share buttons ===
+Contributors: alimuzzamanalim
+Author: Md. Alimuzzaman Alim
+Donate link: none
+Tags: Social-share, Social-share-button, Social, share, button, widget, translation ready, facebook share, google plus share, linkedin share, social media share, social sharing, Social-share-button, twitter share.
+Requires at least: 3.0.0
+Tested up to: 4.0.1
+Version: 2.0.3.1
+License: GPLv2
+Stable tag: 2.0.3.1
+
+Are you searching for a lightweight sharing plugin? You are in right place. This pluign not use JavaScript.
+ 
+== Description ==
+For live demo and question <a target="_blank" href="http://www.zm-tech.net/wp-plugins/html-social-share-buttons/" rel="follow">Live demo</a>.<br/>
+            	Designed By Hakan Ertan <a target="_blank" href="https://hakan-ertan.com/" rel="follow">www.hakan-ertan.com</a>
+
+<strong>New feature:</strong><br />
+<ul>
+	<li>New icons</li>
+	<li>Visual composer support</li>
+	<li>Short code generator</li>
+	<li>Opiton to add bottom of post.</li>
+	<li>PHP code generator to use in theme</li>
+	<li>Translation ready. (Help me to translate to your language.)</li>
+	<li>3 Different button set.</li>
+	<li>You will be able to display share button horizontally on the left or right side.</li>
+	<li>You can develop add-on for this plugin with your custom icons.</li>
+</ul>
+
+<strong>Feature:</strong><br />
+<ul>
+	<li>Html CSS Social share buttons widget Plugin WordPress.</li>
+	<li>It show lite/simple share button only with html css.</li>
+	<li>It's does not using any JavaScript whats another do.</li>
+	<li>It's load only extra 10-11 KB total on your site.</li>
+	<li>There are top share button in this plugin.</li>
+	<li>It's use only one image instead of 5.(Will be back in future release)</li>
+	<li>So only one extra HTTP request will occur.(Will be back in future release)</li>
+</ul>
+Added ability to pickup first image from post for use as thumbnail. ( NEW )
+
+<strong>FEATURE::</strong>
+
+
+<ol>
+	<li>Share with Facebook.</li>
+	<li>Share with Twitter.</li>
+	<li>Share with LinkedIn.</li>
+	<li>Share with Google Plus.</li>
+	<li>Bookmark with Google Bookmark.</li>
+	<li>Share with Email.</li>
+	<li>Share with Pinterest</li>
+</ol>
+
+== Installation ==
+1. At first activate the plugin.
+
+2. There are two way to use this share button. You can use as widget or on the left site.
+
+3. You get a option panel under "Appearance" menu called "Html Social Share".
+
+4. By going to option panel:
+	a) You will able to enable/disable widget.
+	b) You will be able to display share button horizontally.
+
+5. Then drag and drop this widget to your sidebar or header banner or footer.
+
+6. That's all. Enjoy this widget.
+
+If you have any question about installation you may visit  <a href="http://www.zm-tech.net/wp-plugins/html-social-share-buttons/" rel="follow">Html shcial share button</a>.
+
+
+== Upgrade Notice ==
+To uptodate please visit <a href="http://www.zm-tech.net/wp-plugins/html-social-share-buttons/" rel="follow">Html shcial share button</a>. A new version can be publish on site before wordpress.
+
+== Screenshots == 
+1. http://www.zm-tech.net/wp-content/plugins/Html%20Social%20share%20buttons/share.png
---
+++ form.php
@@ -0,0 +1,127 @@
+<?php
+class zm_form{
+	
+	public $options;
+	
+	function __construct($options = ""){
+		global $zm_sh_default_options;
+		$this->options = $options ? $options : get_option("zm_shbt_fld", $zm_sh_default_options);
+	}
+	
+	function text($id, $label, $name = false, $value = false){
+		$name	= $name		? $name		: "zm_shbt_fld[$id]";
+		$value	= $value	? $value	: $this->options[$id];
+		echo "<div class='row'>";
+			echo "<label for='$id' style='width:140px;line-height: 37px;'>$label</label>";
+			echo "<input type='text' id='$id' name='$name' value='$value' style='width: 278px;height: 33px;	background-color: #ffffff;border: 1.2px solid #B8B8B8;' >";
+		echo "</div>";
+	}
+	
+	
+	function checkbox($id, $label, $name = '', $selected=false, $class = 'toggle-check', $yes = "", $no = ""){
+		$yes = $yes?$yes:__("Yes", "zm_sh");
+		$no = $no?$no:__("No", "zm_sh");
+		$chk = $selected===false?checked($this->options[$id], true, false):$selected;
+		$name = $name ? $name : "zm_shbt_fld[$id]";
+		echo "<div class='row'>";
+			echo "<label for='$id'>$label</label>";
+			echo "<input name='$name' id='$id' $chk type='checkbox' value='1' data-id='$id' />";
+			echo "<span class='for_label'>";
+				echo "<label for='$id' class='$class' data-on='$yes' data-off='$no'></label>";
+			echo "</span>";
+		echo "</div>";
+	}
+	
+	function show_on($id, $label, $selected=false, $class = 'toggle-check', $yes = "", $no = ""){
+		$options = $this->options;
+		$iconset = zm_sh_get_iconset($options['iconset']);
+		$yes	= $yes?$yes:__("Yes", "zm_sh");
+		$no		= $no?$no:__("No", "zm_sh");
+		$name	= "zm_shbt_fld[show_in][$id]";
+		$name_1	= "zm_shbt_fld[$id]";
+		$chk	= $options['show_in'][$id]?'checked="checked"':'';
+		if($chk and !$circle and !$square)
+			$circle = 'checked="checked"';
+		echo "<div class='row toggle'>";
+			echo "<label for='$id'>$label</label>";
+			echo "<input id='$id' $chk type='checkbox' name='{$name}' value='1'/>";
+			echo "<span class='for_label'>";
+				echo "<label for='$id' class='$class' data-on='$yes' data-off='$no'></label>";
+				
+				echo "<div class='row show_on' style='margin-top:50px'>";
+					foreach($iconset['types'] as $type){
+						$selected = checked($options[$id], $type, false);
+						echo "<input type='radio' id='$id-$type' name='$name_1' value='$type' $selected >";
+						echo "<label for='$id-$type'><img src='". zm_sh_url_assets_img . $id . "-$type.png'></label>";
+					}
+				echo "</div>";
+			echo "</span>";
+			//print_r($options);
+		echo "</div>";
+	}
+	
+	function icon_fields($label, $label_prefix, $class = 'toggle-check', $yes = "", $no = ""){
+		$icons = zm_sh_get_icons($this->options['iconset']);
+		echo "<div class='row' style='margin-bottom:20px'>";
+			echo "<h2>$label</h2>";
+		echo "</div>";
+		foreach($icons as $name=>$id){
+			$chk = checked($this->options['icons'][$id], true, false);
+			$this->checkbox('icon_'.$id, $label_prefix.' '.$name, "zm_shbt_fld[icons][$id]", $chk, $class, $yes, $no, $id);
+		}
+	}
+	
+	function icon_fields_widget($id, $name, $selected_icons, $label, $label_prefix){
+		$icons = zm_sh_get_icons($this->options['iconset']);
+		echo "<div class='row' style='margin-bottom:20px'>";
+			echo "<h2>$label</h2>";
+		echo "</div>";
+		$selected_icons = $selected_icons?array_keys($selected_icons):array();
+		foreach($icons as $_name=>$_id){
+			if(is_array($selected_icons)){
+				$chk = in_array($_id, $selected_icons)?"checked='checked'":"";
+			}
+			$this->checkbox($id.'_'.$_id, $label_prefix.' '.$_name, $name . "[$_id]", $chk);
+		}
+	}
+	
+	function dropdown($id, $label, $items, $name=false, $selected=false){
+		echo "<div class='row'>";
+			echo "<label for='$id'>$label</label>";
+			echo "<select id='$id' name='$name'>";
+			foreach($items as $item){
+				$selec = selected($selected, $item, false);
+				echo "<option value='$item' $selec>".ucwords($item)."</option>";
+			}
+			echo "</select>";
+		echo "</div>";
+	}
+	
+	function _select_iconset($id, $label, $items, $name=false, $selected=false){
+		$name = $name ? $name : "zm_shbt_fld[$id]";
+		$selected = $selected ? $selected : $this->options[$id];
+		$iconset = zm_sh_get_current_iconset();
+		echo "<div class='row'>";
+			echo "<label for='$id'>$label</label>";
+			echo "<select id='$id' name='$name'>";
+			foreach($items as $i_id=>$i_name){
+				$selec = selected($selected, $i_id, false);
+				echo "<option value='{$i_id}' $selec>{$i_name}</option>";
+			}
+			echo "</select>";
+			?>
+			<div class="button-style-img">
+				<img src="<?php echo $iconset['url'].$iconset['preview_img']; ?>" alt="" class="" />
+			</div>
+			<?php
+		echo "</div>";
+	}
+	
+	function select_iconset($id, $label, $name=false, $selected=false ){
+		$iconsets = zm_sh_get_iconset_list();
+		$this->_select_iconset($id, $label, $iconsets, $name, $selected );
+	}
+	
+	
+	
+}
