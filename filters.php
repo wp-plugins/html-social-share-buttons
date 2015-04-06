2739b3183f6cab755f76ae934a389a54ce5d14b4
alimuzzamanalim <alimuzzamanalim@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-06 16:03:47 +0000

    Added url encode again. requested on https://wordpress.org/support/topic/html-valid?replies=2#post-6783602
    git-svn-id: https://plugins.svn.wordpress.org/html-social-share-buttons/trunk@1128818 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- filters.php
+++ filters.php
@@ -8,13 +8,30 @@ class zm_sh_filters{
 	}
 	
 	function zm_sh_placeholder($item){
-		$title = get_the_title();
-		$parmalink = zm_sh_curentPageURL();
+		$title		= get_the_title();
+		$parmalink	= zm_sh_curentPageURL();
 		$description = get_bloginfo ( 'description' );
-		$item = str_replace( "%%permalink%%",	$parmalink,		$item);
-		$item = str_replace( "%%title%%",		$title,			$item);
-		$item = str_replace( "%%description%%",	$description,	$item);
+		$image_url	= $this->image_url($parmalink);
+		$item 		= str_replace( "%%permalink%%",		urlencode($parmalink),	$item);
+		$item 		= str_replace( "%%title%%",			urlencode($title),		$item);
+		$item 		= str_replace( "%%description%%",	urlencode($description),$item);
+		$item 		= str_replace( "%%imageurl%%",		urlencode($image_url),	$item);
 		return $item;
+	}
+	
+	function image_url($url) {
+		$imageurl = urlencode( wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) );
+		
+		if(!$imageurl){
+			$postid = url_to_postid( $url );
+			$post = get_post( $postid, "OBJECT" );
+			$first_img = '';
+			ob_start();
+			ob_end_clean();
+			$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', do_shortcode($post->post_content), $matches);
+			$imageurl = $matches[1][0];
+		}
+		return $imageurl;
 	}
 }
 
